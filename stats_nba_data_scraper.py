946e2e8664f84c2a0e318efea0d611cfdca85adf
Shreyas Chandrashekaran <svc3@illinois.edu>
2022-05-25 23:59:44 -0700

    fixed deprecated headless parameter, got headless firefox working. constructed current working URL string based on current date

--- stats_nba_data_scraper.py
+++ stats_nba_data_scraper.py
@@ -3,7 +3,13 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from CONST_season_start_end_dates import SEASON_START_END_DATES
 
-browser = Firefox(options=Options().set_headless())
+# URL and parsing method could break at any time due to web scraping being used #
+CONST_URL = "https://www.nba.com/stats/gamebooks/?Date="
+
+opts = Options()
+# toggle the below to see / not see the browser carrying out transactions -- makes things longer if off, but good for testing
+opts.headless = True
+browser = Firefox(options=opts)
 
 def iterate_single_season(season_string):
     if season_string not in SEASON_START_END_DATES.keys():
@@ -16,15 +22,16 @@ def iterate_single_season(season_string):
 
     while curr_date < end_date:
         # do some actions with the current date
+        url = CONST_URL + curr_date.strftime('%m') + "%2F" + curr_date.strftime('%d') + "%2F" + curr_date.strftime('%Y')
+        print(url)
+        break
         # business logic happens here
-
-        #increment the day
-        pass
         # %Y to get the year as an integer
         # %d for zero padded day
         # %m for zero padded month
         print(curr_date)
 
+        #increment the day
         curr_date += timedelta(days = 1)
 
 iterate_single_season('2011-12')

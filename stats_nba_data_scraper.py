1cd9b8467aa71b5eb493838d075157661f4d23c2
Shreyas Chandrashekaran <svc3@illinois.edu>
2022-05-25 23:35:21 -0700

    updated year logic due to mismatch in return types. added new error if year not found. also updated while loop in iterate_single_season to make sense / loop through entire season

--- stats_nba_data_scraper.py
+++ stats_nba_data_scraper.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from CONST_season_start_end_dates import SEASON_START_END_DATES
@@ -5,13 +6,15 @@ from CONST_season_start_end_dates import SEASON_START_END_DATES
 browser = Firefox(options=Options().set_headless())
 
 def iterate_single_season(season_string):
-    (start_date, end_date) = SEASON_START_END_DATES.get(season_string, "Season " + season_string + " dates not found")
+    if season_string not in SEASON_START_END_DATES.keys():
+        raise ValueError("Specified year start/end dates could not be found: " + season_string)
+    (start_date, end_date) = SEASON_START_END_DATES.get(season_string)
 
     curr_date = start_date
 
     print(curr_date.strftime('%d'))
 
-    while curr_date.day <= end_date.day and curr_date.year <= end_date.year:
+    while curr_date < end_date:
         # do some actions with the current date
         # business logic happens here
 
@@ -20,5 +23,8 @@ def iterate_single_season(season_string):
         # %Y to get the year as an integer
         # %d for zero padded day
         # %m for zero padded month
+        print(curr_date)
 
-iterate_single_season('2010-11')
+        curr_date += timedelta(days = 1)
+
+iterate_single_season('2011-12')

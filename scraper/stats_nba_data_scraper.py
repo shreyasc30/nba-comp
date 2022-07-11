c92bc6f151f97f6df59a41dd3849bea98222ce80
Shreyas Chandrashekaran <svc3@illinois.edu>
2022-07-10 22:11:25 -0700

    wrote multiple revisions of business logic, too clunky at the moment. need a few more revisions to get to a usable scraper. test run kicked off

--- scraper/stats_nba_data_scraper.py
+++ scraper/stats_nba_data_scraper.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from CONST_season_start_end_dates import SEASON_START_END_DATES
+from CONST_team_names_since_2011 import TEAM_NAMES
 
 import pandas as pd
 
@@ -46,9 +47,17 @@ def iterate_single_season(season_string, df):
     # we want to go through these games and populate 2 data entries per game (winning team, losing team)
     # once the dataframe is fully populated, we are done iterating through an entire season. So we can return the df
 
+    missing_ = []
     for url in game_urls:
-        df = analyze_single_game_url(season_string, url, df)
-
+        # print(url)
+        (errcode, df) = analyze_single_game_url(season_string, url, df)
+        if errcode != 1:
+            missing_.append((errcode, url))
+    
+    print(missing_)
+    with open('missing.txt', 'w') as f:
+        for item in missing_:
+            f.write("%s\n" % item)
     return df
 
 
@@ -80,18 +89,98 @@ def get_single_gameday_urls(season_string, url, game_urls):
     # TODO: improve sleep mechanism to be dynamic similar to the above to cut down wasted time
     sleep(5)
 
+    # TODO: save the date to add to the return value of this function
+    # unique index of each example will be date.1, date.2 etc.
+
     interactive_box_score_elements = browser.find_elements_by_partial_link_text("Interactive Box Score")
 
     for elem in interactive_box_score_elements:
         link = elem.get_attribute('href')
-        game_urls.append(link)
+        if "gleague" not in link:
+            game_urls.append(link)
     return game_urls
 
 def analyze_single_game_url(season_string, url, df):
     # TODO: implement me !
+    browser.get(url)
 
-    pass
+    print(url)
+    # simplistic sleep because of intermittent error -- find a way too streamline this process
+    sleep(5)
+    try :
+        two_tables_parent = browser.find_elements_by_class_name(name="MaxWidthContainer_mwc__2OXc5")[1]
+
+    except (IndexError):
+        return ((-1, df))
+
+    team_names_player_names_data = two_tables_parent.find_elements_by_class_name('block')
     
+    # use the below to see what information is available to you with this one line
+    # this method can be changed / overridden to extract individual player statistics at the time of writing
+    # using the below / changing from here on out
+    # for team_name in team_names_player_names_data:
+    #     print(team_name.text)
+    
+    try:
+        home_team_name = team_names_player_names_data[0].text
+        away_team_name = team_names_player_names_data[4].text
 
-df = pd.DataFrame()
-iterate_single_season('2011-12', df)
+
+        home_team_data_dump = team_names_player_names_data[1].text
+        away_team_data_dump = team_names_player_names_data[5].text
+    
+    except(IndexError):
+        return ((-3, df))
+
+    # if the away team data is not in the most common spot, we have to find it
+    # we know the team data is the entry right after the name, so we can look for the name
+    away_team_idx = 2
+    while away_team_name.upper() not in TEAM_NAMES:
+        away_team_name = team_names_player_names_data[away_team_idx].text
+        if away_team_idx == len(team_names_player_names_data):
+            print("Could not find away team name, example skipped: " + url)
+            return (-2, df)
+        away_team_idx += 1
+        away_team_data_dump = team_names_player_names_data[away_team_idx].text
+
+
+    # for tnpnd in team_names_player_names_data:
+    #     print(tnpnd.text)
+    #     print("\n\n")
+
+    # if you want individual player data, redirect some logic here to grab it from the above variables
+    # TODO: add support (perhaps with a flag / full CLI support) for extracting player data
+    
+    home_team_data_dump = home_team_data_dump.replace('/n', ',')
+    away_team_data_dump = away_team_data_dump.replace('/n', ',')
+    home_totals_only = home_team_data_dump[home_team_data_dump.rfind("TOTALS"):].split()
+
+    # the below logic was created to handle tests for several special cases documented and dealt with above
+    away_totals_found = away_team_data_dump.rfind("TOTALS")
+    away_totals_only = away_team_data_dump[away_totals_found:].split()
+    home_totals_only[0], away_totals_only[0] = home_team_name, away_team_name
+
+    # print(away_team_data_dump)
+    #print(away_totals_only)
+
+    # print(season_string + " " + home_team_name + " vs. " + away_team_name + " link:" + url)
+    len_df = len(df)
+    df.loc[len_df] = home_totals_only
+    df.loc[len_df + 1] = away_totals_only
+
+    return (1, df)
+
+
+
+    
+    
+urls = get_single_gameday_urls('2011-12', 'https://www.nba.com/stats/gamebooks/?Date=12%2F29%2F2011', [])
+df = pd.DataFrame(columns=["name", "fgm", "fga", "fgperc", "3pm", "3pa", "3pperc", "ftm", "fta", "ftperc", "oreb", "dreb", "reb", "asst", "stl", "block", "to", "fouls", "pts", "plus-minus"])
+df = iterate_single_season('2011-12', df)
+
+# for url in urls:
+#     df = analyze_single_game_url('2011-12', url, df)
+
+print(df.head())
+
+# analyze_single_game_url('2011-12', 'https://www.nba.com/game/cle-vs-uta-0021100139/box-score', df)
